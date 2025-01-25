eb6879bbbe734706fd5824d11a4ab719d6d7bf82
Canoi <canoimaster@gmail.com>
2025-01-25 12:53:32 -0300

    updated msvc triple and trying to compile a x86 version;

--- msvcbuild.bat
+++ msvcbuild.bat
@@ -1,10 +1,21 @@
-cmake -B build
+cmake -B build -G "Visual Studio 17 2022" -A x64
 cmake --build build --config Release
-if not exist "%CD%\dist\windows-msvc-v143\bin" mkdir "%CD%\dist\windows-msvc-v143\bin"
-if not exist "%CD%\dist\windows-msvc-v143\lib" mkdir "%CD%\dist\windows-msvc-v143\lib"
-copy "%CD%\build\Release\lua.exe" "%CD%\dist\windows-msvc-v143\bin\lua.exe"
-copy "%CD%\build\Release\luac.exe" "%CD%\dist\windows-msvc-v143\bin\luac.exe"
-copy "%CD%\build\Release\*.lib" "%CD%\dist\windows-msvc-v143\lib\"
-copy "%CD%\build\Release\*.dll" "%CD%\dist\windows-msvc-v143\lib\"
+if not exist "%CD%\dist\x64-windows-msvc\v143\bin" mkdir "%CD%\dist\x64-windows-msvc\v143\bin"
+if not exist "%CD%\dist\x64-windows-msvc\v143\lib" mkdir "%CD%\dist\x64-windows-msvc\v143\lib"
+copy "%CD%\build\Release\lua.exe" "%CD%\dist\x64-windows-msvc\v143\bin\lua.exe"
+copy "%CD%\build\Release\luac.exe" "%CD%\dist\x64-windows-msvc\v143\bin\luac.exe"
+copy "%CD%\build\Release\*.lib" "%CD%\dist\x64-windows-msvc\v143\lib\"
+copy "%CD%\build\Release\*.dll" "%CD%\dist\x64-windows-msvc\v143\lib\"
+
+rmdir /s /q build
+
+cmake -B build -G "Visual Studio 17 2022" -A x86
+cmake --build build --config Release
+if not exist "%CD%\dist\x86-windows-msvc\v143\bin" mkdir "%CD%\dist\x86-windows-msvc\v143\bin"
+if not exist "%CD%\dist\x86-windows-msvc\v143\lib" mkdir "%CD%\dist\x86-windows-msvc\v143\lib"
+copy "%CD%\build\Release\lua.exe" "%CD%\dist\x86-windows-msvc\v143\bin\lua.exe"
+copy "%CD%\build\Release\luac.exe" "%CD%\dist\x86-windows-msvc\v143\bin\luac.exe"
+copy "%CD%\build\Release\*.lib" "%CD%\dist\x86-windows-msvc\v143\lib\"
+copy "%CD%\build\Release\*.dll" "%CD%\dist\x86-windows-msvc\v143\lib\"
 
 rmdir /s /q build
