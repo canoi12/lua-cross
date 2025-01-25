ef21f832976ace1c3e6b6138d4761c8cc3a734d5
Canoi <canoimaster@gmail.com>
2025-01-25 16:35:59 -0300

    added x64 windows msvc luajit build;

--- luajit/msvcbuild.bat
+++ luajit/msvcbuild.bat
@@ -4,10 +4,18 @@ call msvcbuild.bat;
 
 cd "%CD%\..\.."
 
-if not exist "%CD%\dist\x86-windows-msvc\v143\bin" mkdir "%CD%\dist\x86-windows-msvc\v143\bin"
-if not exist "%CD%\dist\x86-windows-msvc\v143\lib" mkdir "%CD%\dist\x86-windows-msvc\v143\lib"
-copy "%CD%\source\src\luajit.exe" "%CD%\dist\x86-windows-msvc\v143\bin\luajit.exe"
-copy "%CD%\source\src\luajit.lib" "%CD%\dist\x86-windows-msvc\v143\lib\"
-copy "%CD%\source\src\lua51.lib" "%CD%\dist\x86-windows-msvc\v143\lib\"
-copy "%CD%\source\src\lua51.dll" "%CD%\dist\x86-windows-msvc\v143\bin\"
-copy "%CD%\source\src\lua51.exp" "%CD%\dist\x86-windows-msvc\v143\lib\"
+set "arch=x86"
+
+if "%1"=="x64" (
+    set "arch=x64"
+)
+
+echo "%arch%"
+
+if not exist "%CD%\dist\%arch%-windows-msvc\v143\bin" mkdir "%CD%\dist\%arch%-windows-msvc\v143\bin"
+if not exist "%CD%\dist\%arch%-windows-msvc\v143\lib" mkdir "%CD%\dist\%arch%-windows-msvc\v143\lib"
+copy "%CD%\source\src\luajit.exe" "%CD%\dist\%arch%-windows-msvc\v143\bin\luajit.exe"
+copy "%CD%\source\src\luajit.lib" "%CD%\dist\%arch%-windows-msvc\v143\lib\"
+copy "%CD%\source\src\lua51.lib" "%CD%\dist\%arch%-windows-msvc\v143\lib\"
+copy "%CD%\source\src\lua51.dll" "%CD%\dist\%arch%-windows-msvc\v143\bin\"
+copy "%CD%\source\src\lua51.exp" "%CD%\dist\%arch%-windows-msvc\v143\lib\"
