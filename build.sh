c73a3bc6bafcb89662944a4373a325d3dbb3fbbb
Canoi <canoimaster@gmail.com>
2025-01-25 12:41:18 -0300

    added emscripten toolchain to build script;

--- build.sh
+++ build.sh
@@ -22,3 +22,13 @@ cp build/lua.exe dist/x86_64-w64-mingw32/bin
 cp build/luac.exe dist/x86_64-w64-mingw32/bin
 
 rm -rf build/
+
+cmake -B build -DCMAKE_TOOLCHAIN_FILE=$HOME/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
+cmake --build build
+mkdir -p dist/wasm32-unknown-emscripten/bin
+mkdir -p dist/wasm32-unknown-emscripten/lib
+cp build/liblua5* dist/wasm32-unknown-emscripten/lib/
+cp build/lua.* dist/wasm32-unknown-emscripten/bin
+cp build/luac.* dist/wasm32-unknown-emscripten/bin
+
+rm -rf build/
